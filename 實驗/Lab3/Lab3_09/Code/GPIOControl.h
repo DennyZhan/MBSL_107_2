cd67aac47e65a72441a20a5aceb13949a63eada3
吳陽生 <wu0306109@gmail.com>
2019-04-27 20:39:37 +0800

    完成期中準備資料

--- 實驗/Lab3/Lab3_09/Code/GPIOControl.h
+++
@@ -1,9 +0,0 @@
-using namespace std;
-
-int ioExport(unsigned int gpio);
-
-int ioUnexport(unsigned int gpio);
-
-int ioSetDir(unsigned int gpio, string dirStatus);  // dirStatus="in", "out"
-
-int ioSetValue(unsigned int gpio, int value);
